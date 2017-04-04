18c8804e14abf810a19fd275049b136a130ea155
Tyler Davis <tyjdavis@outlook.com>
2017-04-03 20:49:01 -0400

    working on right answer function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
-
 function Question (text, list_of_choices, correct_answer) {
   this.main = text;
   this.choices = list_of_choices;
   this.answer = correct_answer;
+
+//come back to below section
   this.userChoice = '';
   this.isCorrect = function () {
     if (this.userChoice === this.answer){
@@ -14,15 +14,31 @@ function Question (text, list_of_choices, correct_answer) {
   }
 }
 
+
 let jsChoices = ['Bacon', 'Pickles', 'Pizza', 'A programming language'];
 
-let firstQuestion = new Question("What is Javascript?", jsChoices, 'A programming language')
+let firstQuestion = new Question("What is Javascript?", jsChoices, 'A programming language') //placeholder question
 
-console.log(firstQuestion);
 
+
+//printing to html
 
 let source = document.querySelector("#quiz-question").innerHTML;
 let template = Handlebars.compile(source);
 let html = template(firstQuestion);
 let destination = document.querySelector('.handlebars-demo');
 destination.innerHTML = html;
+
+
+
+function rightAnswer (event) {
+  event.preventDefault();
+  console.log('ok');
+  let answer = document.querySelector('radio'); //not able to select individual answer with how it's set up in html
+  console.log(answer)
+
+}
+
+
+let userClick = document.querySelector('form');
+userClick.addEventListener("submit", rightAnswer)
