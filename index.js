cab40275f00fe01693d0cc132ade8a134a16f26b
Tyler Davis <tyjdavis@outlook.com>
2017-04-05 16:24:24 -0400

    trivia api added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function Question (text, answer) {
 }
 
 
+
+
 //Multiple Choice constructor
 
 function MultipleChoiceQuestion (text, answer, choices) {
@@ -37,6 +39,7 @@ function MultipleChoiceQuestion (text, answer, choices) {
 
 
 
+/*
 
 //Short Answer constructor
 
@@ -75,3 +78,56 @@ ShortAnswerQuestion.prototype.display = function () {
 let q1 = new MultipleChoiceQuestion('1. Where is the best place to insert the JavaScript <script> tag in your html?','the bottom of the <body> section', ['the bottom of <head> section', 'the bottom of the <body> section', 'the <title> section', 'does not matter']);
 let q2 = new ShortAnswerQuestion('2. How many days did it take Brendan Eich to create JavaScript in 1995?','10');
 [q1, q2].forEach(question => question.display());
+
+*/
+
+
+
+fetch('https://opentdb.com/api.php?amount=10&category=20&difficulty=easy')
+.then(response => response.json())
+.then(jsonData => jsonData.results) //returns an array of results
+.then(getQuestionsFromAPI)
+.then(displayQuestion)
+
+
+function getQuestionsFromAPI (arr) {
+return arr.map(function (object) {   //object is the elements within the array
+  let choices = object.incorrect_answers;
+  choices.push(object.correct_answer);
+  let newQuestion = new MultipleChoiceQuestion(object.question, object.correct_answer, choices)
+  return newQuestion;
+})
+}
+
+function displayQuestion (arr) {
+  arr.forEach(question => question.display());
+}
+
+
+
+
+let url = "http://putsreq.com/D0b3ymZkJOmQwdpvx9Tp";
+
+function fetchInit (data) {
+  return {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+}
+
+let form = document.querySelector('form');
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  let order = {
+    orderName: form.querySelector('input[name=firstName]').value,
+    theMeal: form.querySelector('input[name=food]').value,
+    soda: form.querySelector('input[name=drink]').value
+  }
+  fetch(url, fetchInit(order))
+  .then(response => response.json())
+  .then(jsonData => console.log(jsonData))
+})
